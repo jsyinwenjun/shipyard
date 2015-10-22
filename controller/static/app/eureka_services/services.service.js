4492f0483a1c403c4c0adf82d6acffb0424362c5
wenjun <wenjun@wenjun-PC>
2015-10-22 17:29:26 +0800

    cc

--- controller/static/app/eureka_services/services.service.js
+++ controller/static/app/eureka_services/services.service.js
@@ -13,6 +13,30 @@
                         .get('/eureka/apps')
                         .then(function(response) {
                         	var res = response.data.applications.application;
+                        	if (!res) res = [];
+                        	var apps = [];
+                        	angular.forEach(res, function(app) {
+                        		var num = 0;
+                        		var addr = "";
+                        		if (app.instance) {
+                        			num = 1;
+                        			if (app.instance.length) {
+                        				num = app.instance.length;
+                        				angular.forEach(app.instance, function(ins) {
+                        					addr += ins.homePageUrl + ",";
+                        				});
+                        				addr = addr.substring(0, addr.length - 1);
+                        			}else {
+                        				addr = app.instance.homePageUrl;
+                        			}
+                        			
+                        		}
+                        		apps.push({
+                        			name: app.name,
+                        			number: num,
+                        			idAddr: addr
+                        		});
+                        	});
                             return res ? res : [];
                         });
                     return promise;
