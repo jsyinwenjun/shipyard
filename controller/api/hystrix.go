2038279adc469c04cdb1cf2f78d4cee0fae76f9b
wenjun <wenjun@wenjun-PC>
2015-10-23 15:29:36 +0800

    cc

--- controller/api/hystrix.go
+++ controller/api/hystrix.go
@@ -3,18 +3,12 @@ package api
 import (
 	"net/http"
 	"net/url"
-	"fmt"
-	"bytes"
 )
 
 func (a *Api) hystrixRedirect(w http.ResponseWriter, req *http.Request) {
 	var err error
 	
 	req.URL, err = url.ParseRequestURI(a.hUrl)
-	c := url.Values{"origin": {fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr)}}
-	postDataBytes := []byte(c)
-	postBytesReader := bytes.NewReader(postDataBytes)
-	req.Body = postBytesReader
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
