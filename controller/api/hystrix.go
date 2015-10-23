69a4ed7b035ec41a4924b041dd99af9b337a70b7
wenjun <wenjun@wenjun-PC>
2015-10-23 14:11:54 +0800

    x

--- controller/api/hystrix.go
+++ controller/api/hystrix.go
@@ -9,7 +9,7 @@ func (a *Api) hystrixRedirect(w http.ResponseWriter, req *http.Request) {
 	var err error
 	
 	req.URL, err = url.ParseRequestURI(a.hUrl)
-	req.URL.Add("origin", a.sopcloudAddr)
+	req.URL.Add("origin", fmt.Sprintf("%s/%s", a.sopcloudAddr, "hystrix.stream"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
