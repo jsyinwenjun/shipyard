225cd6034627efa0738de18f92140788738594fe
wenjun <wenjun@wenjun-PC>
2015-10-26 10:53:47 +0800

    cc

--- controller/api/hystrix.go
+++ controller/api/hystrix.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 	"net/url"
 	"encoding/json"
+	"fmt"
 )
 
 type CloudAddr struct {
-    addr string
+    addr string `json:"addr,omitempty"`
 }
 
 func (a *Api) hystrixRedirect(w http.ResponseWriter, req *http.Request) {
@@ -25,7 +26,7 @@ func (a *Api) getCloudAddr(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 
 	sopcloudAddr := CloudAddr{
-			addr: "asdasd",//fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr),
+			addr: fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr),
 	}
 	if err := json.NewEncoder(w).Encode(sopcloudAddr); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
