bbbf45d1989f6cefc4befb627ae623034a5efb77
wenjun <wenjun@wenjun-PC>
2015-10-26 09:42:25 +0800

    cc

--- controller/static/app/eureka_services/config.routes.js
+++ controller/static/app/eureka_services/config.routes.js
@@ -14,7 +14,7 @@
 			    templateUrl: 'app/eureka_services/services.html',
                             controller: 'EurekaServicesController',
                             controllerAs: 'vm',
-                            authenticate: 'true',
+                            authenticate: true,
                             resolve: {
                                 eureka_services: ['EurekaServicesService', '$state', '$stateParams', function (EurekaServicesService, $state, $stateParams) {
                                     return EurekaServicesService.list().then(null, function(errorData) {	                            
