3d2597c21303de463c4ba41b9f43d2861dcb190e
wenjun <wenjun@wenjun-PC>
2015-10-26 15:13:11 +0800

    cc

--- controller/static/app/hystrix/hystrix.controller.js
+++ controller/static/app/hystrix/hystrix.controller.js
@@ -33,8 +33,8 @@
         		
         		if(stream != undefined) {
         			
-        			var commandStream = "${contextPath}/proxy.stream?origin=" + stream;
-        			var poolStream = "${contextPath}/proxy.stream?origin=" + stream;
+        			var commandStream = location.host + "/proxy.stream?origin=" + stream;
+        			var poolStream = location.host + "/proxy.stream?origin=" + stream;
         			
         			$('#title_name').html("Hystrix Stream: " + decodeURIComponent(stream))
         		}
