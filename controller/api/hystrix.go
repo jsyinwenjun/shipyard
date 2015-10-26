a90e4f19ea5196cc0923a93284ae20b9111c26c4
wenjun <wenjun@wenjun-PC>
2015-10-26 13:52:57 +0800

    cc

--- controller/api/hystrix.go
+++ controller/api/hystrix.go
@@ -5,11 +5,10 @@ import (
 	"net/url"
 	"encoding/json"
 	"fmt"
-	log "github.com/Sirupsen/logrus"
 )
 
 type CloudAddr struct {
-    addr string `json:"addr"`
+    Address string `json:"addr"`
 }
 
 func (a *Api) hystrixRedirect(w http.ResponseWriter, req *http.Request) {
@@ -26,14 +25,7 @@ func (a *Api) hystrixRedirect(w http.ResponseWriter, req *http.Request) {
 func (a *Api) getCloudAddr(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	var sopcloudAddr CloudAddr
-	sopcloudAddr.addr = fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr)
-	body, err := json.Marshal(sopcloudAddr)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	log.Debugf("cloud addr: %s", fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr))
-	log.Debugf("cloud addr: %s", body)
+	sopcloudAddr.Address = fmt.Sprintf("%s/hystrix.stream", a.sopcloudAddr)
 	
 	if err := json.NewEncoder(w).Encode(sopcloudAddr); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
